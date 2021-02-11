3eeb087273f8687f1c48976163254ffb3f555d6e
mykereel <mgmartnz@icloud.com>
2021-02-11 12:06:09 -0700

    added webmanifest and service worker files

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,41 @@
+// Requires in Express Router
 const router = require("express").Router();
+// Requires in Transaction.js
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+// POST
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+// POST
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
+// GET
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
+// Makes "router" accessible through app
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,37 @@
+// Handles server requests
 const express = require("express");
+// Middleware logger for request details
 const logger = require("morgan");
+// Asynchronous environment for MongoDB
 const mongoose = require("mongoose");
+// Decreases the downloadable amount of data that's served to users
 const compression = require("compression");
 
+// Sets Server 3000 to PORT
 const PORT = 3000;
 
+// Sets Express connection to "app"
 const app = express();
 
+// Morgan Connection
 app.use(logger("dev"));
-
+// Compression Connection
 app.use(compression());
+// Express Connections
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// Mongoose Connection
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
-// routes
+// Routes
 app.use(require("./routes/api.js"));
 
+// Server Listener
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
